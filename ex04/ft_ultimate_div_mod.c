5fa2ec7ce5673b1ee678e8e870aa720a9391beba
Muhammad Zarif Abd Raman <mabd-ram@u81z09s06.42kl.edu.my>
2024-05-02 22:17:32 +0800

    ex07 remove

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -6,15 +6,15 @@
 /*   By: mabd-ram <mabd-ram@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 16:33:08 by mabd-ram          #+#    #+#             */
-/*   Updated: 2024/04/30 17:26:40 by mabd-ram         ###   ########.fr       */
+/*   Updated: 2024/05/02 21:32:23 by mabd-ram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	int temp;
+	int	temp;
 
 	temp = *a;
 	*a = temp / *b;
-	*b = temp % *b;	
+	*b = temp % *b;
 }
